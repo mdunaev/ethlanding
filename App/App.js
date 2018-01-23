48251ac5e7e9a0c047f8c11e35ed4c1dd05f0abb
Mikhail Dunaev <dunaev.m@gmail.com>
2018-01-23 19:34:36 +0200

    logo

--- App/App.js
+++ App/App.js
@@ -1,4 +1,5 @@
 import p5 from "p5";
+import Web3 from 'web3';
 
 const width = 1000;
 const height = 1000;
@@ -6,48 +7,88 @@ const height = 1000;
 new p5();
 
 window.setup = () => {
-    createCanvas(1000, 1000);
+    createCanvas(width, height);
 }
 
 window.draw = () => {
-    fill(250, 250, 250);
-    strokeWeight(0);
-    rect(0, 0, 1000, 1000);
-    fill(0, 0, 0, 40);
-
-    dots = dots.map(v => {
-        strokeWeight(0);
-        ellipse(v.x, v.y, 5, 5);
-        if (v.x > width || v.x < 0) v.speedX = -v.speedX;
-        if (v.y > height || v.y < 0) v.speedY = -v.speedY;
-        v.x += v.speedX;
-        v.y += v.speedY;
-        dots.map(v2 => {
-            const distance = distance2d(v.x, v.y, v2.x, v2.y);
-            if (distance > 200) return;
-            strokeWeight((1 - distance / 200) / 2);
-            line(v.x, v.y, v2.x, v2.y);
-        });
-        return v;
-    });
 
 }
 
-const randomTo = (to) => Math.random() * to;
+const web3 = new Web3();
+const logoEl = document.querySelector(".logo");
 
-const teeterTotter = (max, current) => {
-    const x2 = current % (max * 2);
-    const x1 = current % max;
-    return (x2 > x1) ? max - x1 : x1;
+web3.setProvider(new web3.providers.HttpProvider('https://api.myetherapi.com/eth'));
+
+const insertO = str => {
+    var index = 4 + Math.round(Math.random() * 4);
+    return str.substr(0, index) + (Math.random() > 0.5 ? 'Õ' : 'õ') + str.substr(index + 1);
+}
+const redarwLogo = () => {
+    web3.eth.getBlockNumber().then(result => {
+        web3.eth.getBlock(result, (err, block) => {
+            const hash = insertO(block.hash.slice(0, 9));
+            const fx = new TextScramble(logoEl);
+            fx.setText(hash);
+        })
+    })
 }
 
-const distance2d = (x1, y1, x2, y2) => Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+class TextScramble {
+    constructor(el) {
+      this.el = el
+      this.chars = 'abcdefghijklmnopqrstuvwxyz1234567890'
+      this.update = this.update.bind(this)
+    }
 
-var dots = new Array(30).fill().map((v, i) => {
-    return {
-        x: randomTo(width),
-        y: randomTo(height),
-        speedX: randomTo(4) - 2,
-        speedY: randomTo(4) - 2,
-    };
-});
+    setText(newText) {
+      const oldText = this.el.innerText
+      const length = Math.max(oldText.length, newText.length)
+      const promise = new Promise((resolve) => this.resolve = resolve)
+      this.queue = []
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || ''
+        const to = newText[i] || ''
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
+        this.queue.push({ from, to, start, end })
+      }
+      cancelAnimationFrame(this.frameRequest)
+      this.frame = 0
+      this.update()
+      return promise
+    }
+
+    update() {
+      let output = ''
+      let complete = 0
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i]
+        if (this.frame >= end) {
+          complete++
+          output += to
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar()
+            this.queue[i].char = char
+          }
+          output += `<span class="dud">${char}</span>`
+        } else {
+          output += from
+        }
+      }
+      this.el.innerHTML = output
+      if (complete === this.queue.length) {
+        this.resolve()
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update)
+        this.frame++
+      }
+    }
+
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+  }
+
+redarwLogo();
+window.setInterval(redarwLogo, 15 * 100 * 5);
